8a8ad0a2ca808ff00e4d275f93b033952eee4eb6
Zhenya <jen9.vovchyk@outlook.com>
2020-03-07 18:20:58 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+
+
+var btn = document.createElement("SPAN");
+document.querySelector('.block1').appendChild(btn);
+btn.innerHTML = "CLICK ME";
+btn.setAttribute('class', 'div1');
+document.body.appendChild(btn);
+console.log(btn);
+
+
+
+
